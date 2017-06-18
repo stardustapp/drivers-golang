62f042d37bf9a02dd60a6b8ebe7f87e7df5fd00f
Daniel Lamando <dan+stardust@danopia.net>
2017-06-17 23:08:51 -0700

    Add AWS SQS "client", doesn't do shit yet

---
+++ aws-sqs-client/functions/init/source.go
@@ -0,0 +1,21 @@
+import (
+  "github.com/aws/aws-sdk-go/aws"
+  "github.com/aws/aws-sdk-go/aws/credentials"
+  "github.com/aws/aws-sdk-go/aws/session"
+  //"github.com/aws/aws-sdk-go/service/sqs"
+)
+
+func InitImpl(input *Credentials) (output *Client) {
+  creds := credentials.NewStaticCredentials(input.AccessKeyID, input.SecretAccessKey, "")
+
+  sess := session.Must(session.NewSessionWithOptions(session.Options{
+    Config: aws.Config{
+      Region:      aws.String(input.Region),
+      Credentials: creds,
+    },
+  }))
+
+  return &Client{
+    session: sess,
+  }
+}

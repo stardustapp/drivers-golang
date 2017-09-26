890478e0b4d7c0c1992a1340a1e73c404bb834f0
Daniel Lamando <apt@danopia.net>
2017-09-25 23:24:35 -0700

    pushjet-dialer: log what the problem is since irc-client doesn't

--- pushjet-client/functions/send-message.go
+++ pushjet-client/functions/send-message.go
@@ -1,13 +1,16 @@
 import pushjet "github.com/geir54/goPushJet"
 import "strconv"
+import "log"
 
 func (svc *Service) SendMessageImpl(msg *Message) string {
   level, _ := strconv.Atoi(msg.Level)
   err := pushjet.SendMessage(svc.Secret, msg.Text, msg.Title, level, msg.Link)
 
   if err != nil {
+    log.Println(err.Error())
     return err.Error()
   } else {
+    log.Println("ok")
     return "ok"
   }
 }

eb18df28cdb2bca832adddf4c87575e35807cbf6
Daniel Lamando <apt@danopia.net>
2017-12-07 21:24:14 -0800

    Add basic coinbase-api-client driver, gets current BTC price

---
+++ coinbase-api-client/functions/fetch-prices.go
@@ -0,0 +1,44 @@
+import "log"
+import "net/http"
+import "time"
+import "encoding/json"
+import "github.com/stardustapp/core/inmem"
+
+var client = &http.Client{Timeout: 5 * time.Second}
+
+func FetchPricesImpl() *PriceInfo {
+  log.Println("Making coinbase request")
+
+  r, err := client.Get("https://api.coinbase.com/v2/prices/USD/spot")
+  if err != nil {
+    log.Println("coinbase HTTP error:", err)
+    return &PriceInfo{}
+  }
+  defer r.Body.Close()
+
+  var resp apiResp
+  err = json.NewDecoder(r.Body).Decode(&resp)
+  if err != nil {
+    log.Println("coinbase JSON decoder error:", err)
+    return &PriceInfo{}
+  }
+
+  info := &PriceInfo{
+    Currency: resp.Data[0].Currency,
+    Prices: inmem.NewFolder("prices"),
+  }
+  for _, line := range resp.Data {
+    log.Println("Coinbase offered", line.Amount, "for 1", line.Base)
+    info.Prices.Put(line.Base, inmem.NewString("price", line.Amount))
+  }
+  return info
+}
+
+type apiResp struct {
+  Data []priceEntry
+}
+type priceEntry struct {
+  Base string
+  Currency string
+  Amount string
+}

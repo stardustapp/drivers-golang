887bec9bf63f354347ed9390a0eb5aea3cf19f7a
Daniel Lamando <apt@danopia.net>
2018-01-14 00:56:47 -0800

    Start a deluge-client driver which can list torrents

---
+++ deluge-client/functions/dial.go
@@ -0,0 +1,38 @@
+import (
+  "log"
+  "fmt"
+
+  "github.com/stardustapp/core/extras"
+  "github.com/stardustapp/core/inmem"
+  "github.com/stardustapp/core/toolbox"
+
+  deluge "github.com/pyed/go-deluge"
+)
+
+func (r *Root) DialImpl(config *DialConfig) string {
+
+  // Return absolute URI to the created session
+  if r.Sessions == nil {
+    // TODO: this should be made already
+    r.Sessions = inmem.NewObscuredFolder("sessions")
+  }
+
+  sessionId := extras.GenerateId()
+  sessionPath := fmt.Sprintf(":9234/pub/sessions/%s", sessionId)
+  sessionUri, _ := toolbox.SelfURI(sessionPath)
+
+  // make the client
+  svc, err := deluge.New(config.URL, config.Password)
+  if err != nil {
+    log.Println("WARN: deluge client failed to launch,", err)
+    return "ERR"
+  }
+  session := &Client{svc}
+  log.Printf("built session %+v", session)
+
+  if ok := r.Sessions.Put(sessionId, session); !ok {
+    log.Println("WARN: Session store rejected us :(")
+    return "ERR"
+  }
+  return sessionUri
+}
---
+++ deluge-client/functions/get-torrents.go
@@ -0,0 +1,44 @@
+import (
+  "log"
+  "strconv"
+
+  "github.com/stardustapp/core/base"
+  "github.com/stardustapp/core/inmem"
+)
+
+func (c *Client) GetTorrentsImpl() base.Folder {
+  torrents, err := c.svc.GetTorrents()
+  if err != nil {
+    log.Println("deluge failed:", err)
+    return nil
+  }
+
+  folder := inmem.NewFolder("torrents")
+  idx := 0
+
+  for _, t := range torrents {
+    ent := &Torrent{
+      Eta: strconv.FormatFloat(t.Eta, 'f', -1, 64),
+      Hash: t.Hash,
+      IsFinished: strconv.FormatBool(t.IsFinished),
+      NumFiles: strconv.FormatFloat(t.NumFiles, 'f', -1, 64),
+      NumPeers: strconv.FormatFloat(t.NumPeers, 'f', -1, 64),
+      NumSeeds: strconv.FormatFloat(t.NumSeeds, 'f', -1, 64),
+      Paused: strconv.FormatBool(t.Paused),
+      Progress: strconv.FormatFloat(t.Progress, 'f', -1, 64),
+      Queue: strconv.FormatFloat(t.Queue, 'f', -1, 64),
+      Ratio: strconv.FormatFloat(t.Ratio, 'f', -1, 64),
+      State: t.State,
+      TorrentName: t.Name,
+      TotalDone: strconv.FormatFloat(t.TotalDone, 'f', -1, 64),
+      TotalPeers: strconv.FormatFloat(t.TotalPeers, 'f', -1, 64),
+      TotalSeeds: strconv.FormatFloat(t.TotalSeeds, 'f', -1, 64),
+      TotalSize: strconv.FormatFloat(t.TotalSize, 'f', -1, 64),
+      TotalUploaded: strconv.FormatFloat(t.TotalUploaded, 'f', -1, 64),
+    }
+    folder.Put(strconv.Itoa(idx), ent)
+    idx += 1
+  }
+
+  return folder
+}
